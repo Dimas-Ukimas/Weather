fef65362021e07f4be66322a8538322a01b8fc12
Dmitriy Butenko <dimas.ukimas@gmail.com>
2025-04-03 18:56:40 +0300

    refactor: change UserMapper by removing unnecessary annotations

--- src/main/java/com/dimasukimas/mapper/UserMapper.java
+++ src/main/java/com/dimasukimas/mapper/UserMapper.java
@@ -5,22 +5,14 @@ import com.dimasukimas.dto.UserResponseDto;
 import com.dimasukimas.entity.User;
 import jakarta.servlet.http.Cookie;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.Named;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
-    @Mapping(target = "password", source = "password", qualifiedByName = "encodePassword")
     User toEntity(UserRequestDto dto);
 
     UserResponseDto toDto(User user, Cookie cookie);
 
-    @Named("encodePassword")
-    default String encodePassword(String rawPassword){
-        return new BCryptPasswordEncoder().encode(rawPassword);
-    }
+
 
 }
