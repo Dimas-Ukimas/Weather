7e34bc6e62819da75315d55cd6dede83971a0830
Dmitriy Butenko <dimas.ukimas@gmail.com>
2025-04-06 17:46:44 +0300

    chore: add OpenWeatherConfig and tweak CookieUtils

--- src/main/java/com/dimasukimas/util/CookieUtils.java
+++ src/main/java/com/dimasukimas/util/CookieUtils.java
@@ -1,18 +1,13 @@
 package com.dimasukimas.util;
 
 import jakarta.servlet.http.Cookie;
-import lombok.experimental.UtilityClass;
-import org.springframework.beans.factory.annotation.Value;
 
 import java.util.UUID;
 
-@UtilityClass
+
 public class CookieUtils {
 
-    @Value("${session.timeout}")
-    private int sessionTimeoutInMinutes;
-
-    public Cookie createCookie(UUID sessionId) {
+    public static Cookie createCookie(UUID sessionId, int sessionTimeoutInMinutes) {
         int cookieMaxAgeInSeconds = 60 * sessionTimeoutInMinutes;
 
         Cookie cookie = new Cookie("userCookie", sessionId.toString());
